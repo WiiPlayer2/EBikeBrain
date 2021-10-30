926e0a7eda8f9d84580dcba695e98150b7f755da
Waldemar Tomme <waldetomme@gmail.com>
2021-10-30 23:09:46 +0200

    Add dummy connect button

---
+++ MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using EBikeBrain.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EBikeBrain
+{
+    internal class MainViewModel
+    {
+        public ICommand ConnectCommand { get; }
+
+        public MainViewModel()
+        {
+            ConnectCommand = new AsyncActionCommand(Connect);
+        }
+
+        private Task Connect(object? arg)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
