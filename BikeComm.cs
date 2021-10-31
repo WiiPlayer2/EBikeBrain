1deb7b54555870f6ff5a0895bf67572898f4c3fc
Waldemar Tomme <waldetomme@gmail.com>
2021-10-31 22:21:30 +0100

    Add BikeComm

---
+++ BikeComm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EBikeBrain
+{
+    internal class BikeComm : IAsyncDisposable
+    {
+        public enum ErrorCode : byte
+        {
+            Ok = 0x01,
+        }
+
+        public enum PasLevel : byte
+        {
+            PAS0 = 0x00,
+            PAS1 = 0x01,
+            PAS2 = 0x0B,
+            PAS3 = 0x0C,
+            PAS4 = 0x0D,
+            PAS5 = 0x02,
+            PAS6 = 0x15,
+            PAS7 = 0x16,
+            PAS8 = 0x17,
+            PAS9 = 0x03,
+        }
+
+        private readonly Stream inputStream;
+
+        private readonly Stream outputStream;
+
+        private readonly SemaphoreSlim semaphoreSlim = new(1);
+
+        private readonly byte[] buffer = new byte[16];
+
+        public BikeComm(Stream inputStream, Stream outputStream)
+        {
+            this.inputStream = inputStream;
+            this.outputStream = outputStream;
+        }
+
+        public async Task<byte[]> Request(int responseLength, params byte[] request)
+        {
+            await semaphoreSlim.WaitAsync();
+
+            try
+            {
+                await outputStream.WriteAsync(request, 0, request.Length);
+                await outputStream.FlushAsync();
+                if (responseLength == 0)
+                    return Array.Empty<byte>();
+
+                var actualResponseLength = await inputStream.ReadAsync(buffer, 0, responseLength);
+                return buffer.Take(actualResponseLength).ToArray();
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
+        }
+
+        public async Task<ErrorCode> GetErrorCode()
+        {
+            var response = await Request(1, 0x11, 0x08);
+            return (ErrorCode)response[0];
+        }
+
+        public async Task<ushort> GetWheelRpm()
+        {
+            var response = await Request(3, 0x11, 0x20);
+            return (ushort)((response[0] * 256u) + response[1]);
+        }
+
+        public Task SetLights(bool isOn)
+            => Request(1, 0x16, 0x1A, (byte) (isOn ? 0xF1 : 0xF0));
+
+        public async Task<byte> GetBatteryPercentage()
+        {
+            var response = await Request(2, 0x11, 0x11);
+            return response[0];
+        }
+
+        public async Task<double> GetAmps()
+        {
+            var response = await Request(2, 0x11, 0x0A);
+            return response[0] / 2.0;
+        }
+
+        public Task SetPasLevel(PasLevel level)
+            => Request(0, WithChecksum(0x16, 0x0B, (byte) level));
+
+        private static byte[] WithChecksum(params byte[] data)
+        {
+            byte checksum = 0;
+            foreach (var b in data)
+                checksum += b;
+            return data.Concat(new[] {checksum}).ToArray();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            semaphoreSlim.Dispose();
+            await inputStream.DisposeAsync();
+            await outputStream.DisposeAsync();
+        }
+    }
+}
--- MainViewModel.cs
+++ MainViewModel.cs
@@ -3,22 +3,44 @@ using Android.Content;
 using EBikeBrain.Helper;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using EBikeBrain.Annotations;
 using Java.Util;
 using Microsoft.Maui;
 
 namespace EBikeBrain
 {
-    internal class MainViewModel
+    internal class MainViewModel : INotifyPropertyChanged
     {
         private BluetoothSocket? currentSocket;
+
+        private double currentRpm;
+
+        private BikeComm? bikeComm;
+
+        private CancellationTokenSource? currentCancellationTokenSource;
+
+        private Task? updateLoopTask;
 
         public ActionCommand ConnectCommand { get; }
 
         public ActionCommand DisconnectCommand { get; }
+
+        public double CurrentRPM
+        {
+            get => currentRpm;
+            set
+            {
+                currentRpm = value;
+                OnPropertyChanged();
+            }
+        }
 
         public MainViewModel()
         {
@@ -33,6 +55,7 @@ namespace EBikeBrain
             var device = adapter?.GetRemoteDevice("C8:C9:A3:D2:E1:CE");
             var bluetoothServiceClass = UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
             currentSocket = device?.CreateRfcommSocketToServiceRecord(bluetoothServiceClass);
+
             if (currentSocket == null)
                 return;
 
@@ -40,6 +63,8 @@ namespace EBikeBrain
             {
                 ConnectCommand.Refresh();
                 await currentSocket.ConnectAsync();
+                bikeComm = new BikeComm(currentSocket.InputStream!, currentSocket.OutputStream!);
+                updateLoopTask = Task.Run(UpdateLoop);
             }
             catch
             {
@@ -50,13 +75,45 @@ namespace EBikeBrain
             DisconnectCommand.Refresh();
         }
 
-        private void Disconnect()
+        private async Task UpdateLoop()
         {
+            currentCancellationTokenSource?.Dispose();
+            currentCancellationTokenSource = new();
+            while (!currentCancellationTokenSource.IsCancellationRequested)
+            {
+                await Task.Delay(250);
+                if (currentCancellationTokenSource.IsCancellationRequested)
+                    return;
+
+                CurrentRPM = await bikeComm!.GetWheelRpm();
+            }
+        }
+
+        private async Task Disconnect()
+        {
+            currentCancellationTokenSource?.Cancel();
+
+            await bikeComm!.DisposeAsync();
             currentSocket?.Close();
             currentSocket = null;
+            bikeComm = null;
+
+            try
+            {
+                await (updateLoopTask ?? Task.CompletedTask);
+            }
+            catch { }
 
             ConnectCommand.Refresh();
             DisconnectCommand.Refresh();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
