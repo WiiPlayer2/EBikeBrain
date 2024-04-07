bcb94c2594a4c5c2f5e64ce64df3271cef44e40c
Waldemar Tomme <waldemar.tomme@bluehands.de>
2024-04-07 16:02:50 +0200

    Add new MAUI app

---
+++ EBikeBrainApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EBikeBrainApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EBikeBrainApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EBikeBrainApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
