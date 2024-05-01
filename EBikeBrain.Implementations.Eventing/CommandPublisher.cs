9212003e9ec6003c7b13a132ca536558d2eb6315
Waldemar Tomme <waldemar.tomme@bluehands.de>
2024-05-01 16:56:26 +0200

    Log every command publication

--- EBikeBrain.Implementations.Eventing/CommandPublisher.cs
+++ EBikeBrain.Implementations.Eventing/CommandPublisher.cs
@@ -1,9 +1,14 @@
 using EBikeBrainApp.Application.Abstractions;
+using EBikeBrainApp.Domain;
 
 namespace EBikeBrain.Implementations.Eventing;
 
-public class CommandPublisher<T>(ICommandHandler<T> commandHandler) : ICommandPublisher<T>
+public class CommandPublisher<T>(ICommandHandler<T> commandHandler, IEventStream<LogEntry> logs) : ICommandPublisher<T>
     where T : notnull
 {
-    public Task Publish(T command) => commandHandler.ExecuteAsync(command);
+    public Task Publish(T command)
+    {
+        logs.Publish(LogEntry.From($">> {command}"));
+        return commandHandler.ExecuteAsync(command);
+    }
 }
