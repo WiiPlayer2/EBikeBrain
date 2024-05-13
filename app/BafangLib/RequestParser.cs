e325befd74dbd86534c2bca7f1229df2d952f43b
Waldemar Tomme <waldemar.tomme@bluehands.de>
2024-05-13 13:58:41 +0200

    Improve buffer length handling

--- app/BafangLib/RequestParser.cs
+++ app/BafangLib/RequestParser.cs
@@ -5,16 +5,17 @@ namespace BafangLib;
 
 public static class RequestParser
 {
+    public const int MAX_REQUEST_LENGTH = 3;
+
+    public const int MIN_REQUEST_LENGTH = 2;
+
     public static ParseResult<Request>? Parse(ReadOnlySpan<byte> buffer) =>
         Parse(buffer, 0, buffer.Length);
 
     public static ParseResult<Request>? Parse(ReadOnlySpan<byte> buffer, int offset, int length)
     {
-        while (true)
+        while (length >= MIN_REQUEST_LENGTH)
         {
-            if (length < 2)
-                return null;
-
             if (length >= 3)
                 if (buffer[offset] == 0x16 && buffer[offset + 1] == 0x0B)
                     return new ParseResult<Request>(new SetPasRequest((Pas) buffer[offset + 2]), offset + 3);
@@ -25,6 +26,8 @@ public static class RequestParser
             offset += 1;
             length -= 1;
         }
+
+        return null;
     }
 }
 
