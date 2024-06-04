c237cf061e9956794a976c61caed610571c4f62e
Waldemar Tomme <waldemar.tomme@bluehands.de>
2024-06-04 12:52:05 +0200

    Replay last event from event stream

--- app/EBikeBrain.Implementations.Eventing/EventStream.cs
+++ app/EBikeBrain.Implementations.Eventing/EventStream.cs
@@ -5,7 +5,13 @@ namespace EBikeBrain.Implementations.Eventing;
 
 public class EventStream<T>(EventBus eventBus) : IEventStream<T> where T : notnull
 {
+    private readonly IObservable<T> eventsWithBuffer = eventBus
+        .OfType<T>()
+        .Replay(1)
+        .RefCount();
+
     public void Publish(T value) => eventBus.OnNext(value);
 
-    public IDisposable Subscribe(IObserver<T> observer) => eventBus.OfType<T>().Subscribe(observer);
+    public IDisposable Subscribe(IObserver<T> observer) => eventsWithBuffer
+        .Subscribe(observer);
 }
